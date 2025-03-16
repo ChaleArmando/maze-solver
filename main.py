b211442d65fc995737d590b7da93a38c2a0ebbe9
ChaleArmando <chalejesus216@gmail.com>
2025-03-16 12:25:34 -0500

    Add Point and Line class, create functionality to draw lines by points

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from Window import *
 
 def main():
     win = Window(800, 600)
+    # Test Lines
+    l1 = Line(Point(1,2), Point(50, 90))
+    win.draw_line(l1, "blue")
+    l2 = Line(Point(92, 156), Point(300, 340))
+    win.draw_line(l2, "red")
     win.wait_for_close()
 
 main()
